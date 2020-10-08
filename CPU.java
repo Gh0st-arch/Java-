d4e34ae5617fab8bc0dd819e47c8740d8729e7a6
Gh0st-arch <72387090+Gh0st-arch@users.noreply.github.com>
2020-10-08 19:47:06 +0800

    Add files via upload

---
+++ CPU.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class CPU {
+    private int speed;
+	int cpu;
+	public void CPU()
+	{
+		
+	}
+	public void CPU(int cpu)
+	{
+		this.cpu = cpu;
+	}
+	void setspeed (int m)
+	{
+		speed = m; 
+	}
+    int getSpeed()
+	{
+		return speed;
+	}
+}
---
+++ HardDisk.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class HardDisk {
+	protected int amount;
+	int HD;
+	public void HardDisk()
+	{
+		
+	}
+	public void HardDisk(int HD)
+	{
+		this.HD = HD;
+	}
+	void setAmount(int m) 
+	{
+		amount = m;
+	}
+	int getAmount()
+	{
+		return amount;
+	}
+
+}
